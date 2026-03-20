e279833a9ebc43a5d9ce84639e86241b2ac17392
pwoprurho <akporurho@yahoo.com>
2026-03-20 17:43:48 +0100

    fix: downgrade reanimated to 3.x and update babel plugins for build stability

--- babel.config.js
+++ babel.config.js
@@ -7,8 +7,8 @@ module.exports = function (api) {
       !isTest && ['module:react-native-dotenv', {moduleName: '@env'}],
       ['@babel/plugin-proposal-decorators', {legacy: true}],
       '@babel/plugin-transform-export-namespace-from', //Zod 4 uses modern JavaScript syntax (export * as) that needs to be transformed by Babel
-      // 'react-native-reanimated/plugin', // this is not needed in 4.x
-      'react-native-worklets/plugin', // must stay last
+      'react-native-reanimated/plugin',
+      'react-native-worklets-core/plugin', // must stay last
     ].filter(Boolean),
   };
 };
